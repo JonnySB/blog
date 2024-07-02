84c8607cac6fd3763a52491b2d1a967c2b56d9a7
jonny brownrigg <jonny.brownrigg@jisc.ac.uk>
2024-07-02 16:34:06 +0100

    added blog model and registered in settings

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) # saved automatically when creating an object
+    updated = models.DateTimeField(auto_now=True) # updated automatically when updating an object
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
