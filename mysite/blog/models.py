399ef337574eb32396037339994b90e46b1c60cd
jonny brownrigg <jonny.brownrigg@jisc.ac.uk>
2024-07-02 17:46:08 +0100

    updated blog model to have status choices

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Publishded'
+
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True) # saved automatically when creating an object
     updated = models.DateTimeField(auto_now=True) # updated automatically when updating an object
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
     class Meta:
         ordering = ['-publish']
@@ -16,4 +25,4 @@ class Post(models.Model):
         ]
 
     def __str__(self):
-        return self.title
+        return str(self.title)
