97c3d0d436ccf078af5c4bc393eb73184e3c0502
jonny brownrigg <jonny.brownrigg@jisc.ac.uk>
2024-07-15 17:18:24 +0100

    add canonical url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,17 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
+
 class PublishedManager(models.Manager):
+
     def get_queryset(self):
-        return (
-            super().get_queryset().filter(status=Post.Status.PUBLISHED)
-        )
+        return (super().get_queryset().filter(status=Post.Status.PUBLISHED))
+
 
 class Post(models.Model):
+
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -22,22 +25,19 @@ class Post(models.Model):
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True) # saved automatically when creating an object
-    updated = models.DateTimeField(auto_now=True) # updated automatically when updating an object
-    status = models.CharField(
-        max_length=2,
-        choices=Status,
-        default=Status.DRAFT
-    )
+    created = models.DateTimeField(auto_now_add=True)  # saved automatically when creating an object
+    updated = models.DateTimeField(auto_now=True)  # updated automatically when updating an object
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
     objects = models.Manager()
     published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish'])
-        ]
+        indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
