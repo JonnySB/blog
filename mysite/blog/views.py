e37190174e4933854a041a3aa8e9c3b108868c37
jonny brownrigg <jonny.brownrigg@jisc.ac.uk>
2024-08-01 10:00:04 +0100

    converted from simple search to trigram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 # external imports
 from taggit.models import Tag
 
@@ -105,6 +105,9 @@ def post_comment(request, post_id):
 
 
 def post_search(request):
+    """
+    Uses Trigram searching
+    """
     form = SearchForm()
     query = None
     results = []
@@ -113,14 +116,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query),
-                ).filter(rank__gte=0.3)
-                .order_by("-rank")
+                    similarity=TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1)
+                .order_by("-similarity")
             )
 
     return render(
